19fe93314da3f219eda5f9e9ecda71b29dae2486
sanjulb <sanjuambhi20@gmail.com>
2024-10-18 15:42:22 +0530

    Handle single number case

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require("./stringCalculator")
 test("return 0 for an emty error", () => {
     expect(add("")).toBe(0)
 })
+
+test("returns the number when single number in the string", () => {
+    expect(add("1")).toBe(1)
+})
