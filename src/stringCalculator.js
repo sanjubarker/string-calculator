6f1c338053a6dbb0e375ae6504f4e024f10c5518
sanjulb <sanjuambhi20@gmail.com>
2024-10-18 16:56:34 +0530

    Added code to Support different delimiters, i.s: semicolon(;)

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,8 +2,15 @@ const add = (numbers) => {
     if (!numbers) return 0
     else if (numbers.length == 1) return parseInt(numbers);
 
-    const formattedNums = numbers.replace(/\n/g, ",");
-    const numArray = formattedNums.split(",");
+    let delimiter = ","
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = parts[0].slice(2);
+        numbers = parts[1];
+    }
+
+    const formattedNums = numbers.replace(/\n/g, delimiter);
+    const numArray = formattedNums.split(delimiter);
 
     return numArray.reduce((sum, current) => sum + parseInt(current || 0), 0);
 }
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -15,3 +15,7 @@ test("return the sum of multiple numbers seperated by commas", () => {
 test("handles new line delimiters between numbers", () => {
     expect(add("1\n2,3")).toBe(6)
 })
+
+test("supports custom delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3)
+})
