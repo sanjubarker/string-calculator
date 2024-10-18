8d265b41914e23de365641410dac82a2173a1419
sanjulb <sanjuambhi20@gmail.com>
2024-10-18 16:35:47 +0530

    handles new line delimiters between numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,9 +2,10 @@ const add = (numbers) => {
     if (!numbers) return 0
     else if (numbers.length == 1) return parseInt(numbers);
 
-    const numArray = numbers.split(",") 
+    const formattedNums = numbers.replace(/\n/g, ",");
+    const numArray = formattedNums.split(",");
 
-    return numArray.reduce((sum, current) => sum + parseInt(current), 0)
+    return numArray.reduce((sum, current) => sum + parseInt(current || 0), 0);
 }
 
 module.exports = add;
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -8,6 +8,10 @@ test("returns the number when single number in the string", () => {
     expect(add("1")).toBe(1)
 })
 
-test("return the sum of multiple numbers seperated by commas.", () => {
+test("return the sum of multiple numbers seperated by commas", () => {
     expect(add("1,5")).toBe(6)
+})
+
+test("handles new line delimiters between numbers", () => {
+    expect(add("1\n2,3")).toBe(6)
 })
