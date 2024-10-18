e7425fe5a81cebe5aff9be55714866f1a4b674a5
sanjulb <sanjuambhi20@gmail.com>
2024-10-18 16:14:45 +0530

    handle any amount of numbers seperated by commas

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,10 @@
 const add = (numbers) => {
     if (!numbers) return 0
     else if (numbers.length == 1) return parseInt(numbers);
+
+    const numArray = numbers.split(",") 
+
+    return numArray.reduce((sum, current) => sum + parseInt(current), 0)
 }
 
 module.exports = add;
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -7,3 +7,7 @@ test("return 0 for an emty error", () => {
 test("returns the number when single number in the string", () => {
     expect(add("1")).toBe(1)
 })
+
+test("return the sum of multiple numbers seperated by commas.", () => {
+    expect(add("1,5")).toBe(6)
+})
